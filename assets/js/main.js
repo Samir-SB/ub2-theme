bb632e484f608968b72ab468036f8dd8aefd50fd
solo054 <bs_solo054@yahoo.fr>
2020-10-13 09:52:25 +0100

    add counter  (satats ) section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,11 @@ var ce = document.querySelector('#current-event');
 var listItem = document.querySelectorAll('.list-grid li');
 var fac = document.querySelector('#faculties');
 
+const counterSection = document.querySelector('#counter');
+const numbers = document.querySelectorAll('.number');
+const speed = 2500;
+const counterOptions = {};
+
 var options = {
   threshold: 0.5,
 };
@@ -16,8 +21,8 @@ observer = new IntersectionObserver(function (entries, observer) {
       mvRight.classList.add('to-right');
       mvLeft.classList.add('to-left');
     } else {
-      mvRight.classList.remove('to-right');
-      mvLeft.classList.remove('to-left');
+      // mvRight.classList.remove('to-right');
+      // mvLeft.classList.remove('to-left');
     }
   });
 }, options);
@@ -35,5 +40,35 @@ observerFade = new IntersectionObserver(function (entries, observer) {
   });
 }, options);
 
+const counterObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      numbers.forEach((number) => {
+        const updateCount = () => {
+          const target = +number.getAttribute('data-number');
+          const count = +number.innerText;
+
+          var inc = (target / speed) * 41.2321;
+          if (count < target) {
+            number.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 50);
+          } else {
+            number.innerText = target;
+          }
+        };
+
+        updateCount();
+      });
+    } else {
+      // Comment on the following lines if you only want to
+      // animate numbers the first time they appear on the screen.
+      numbers.forEach((number) => {
+        number.innerText = 0;
+      });
+    }
+  });
+}, counterOptions);
+
 observerFade.observe(fac);
 observer.observe(ce);
+counterObserver.observe(counterSection);
